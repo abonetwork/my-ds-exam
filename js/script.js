55c338aeaeb0b7ab2b9ab0980383db22065977d0
anonymous_mind <litoabonjr@gmail.com>
2019-10-04 10:35:29 +0800

    slider and slider images

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+$(document).ready(function() {
+  console.log("running");
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
+
+  $(".slider1").slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    // autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    fadeSpeed: 1000
+  });
+});
+
 // NAVIGATION SHOW/HIDE
 $("nav ul").hide();
 
